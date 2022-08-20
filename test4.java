f01001bbbc73e39f813bee7c2c506ed851c0f552
Sam Yuan <shaominyuan@yahoo.com>
2022-08-19 22:26:53 -0700

    test again

---
+++ test4.java
@@ -0,0 +1,18 @@
+class Main {
+
+    public static void main(String[] args) {
+      
+      System.out.println("Enter two numbers");
+      int first = 10;
+      int second = 20;
+      
+      System.out.println(first + " " + second);
+  
+      // add two numbers
+      int sum = first + second;
+      if (first > second)
+      System.out.println("OK");
+
+      System.out.println("The sum is: " + sum);
+    }
+  }
